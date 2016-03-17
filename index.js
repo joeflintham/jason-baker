1e8afbb578b6d46b1d3bbcf415f120e6f15878ca
Joe Flintham <joeflintham@gmaail.com>
2016-03-17 19:12:31 +0000

    Jason Baker as a promise

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict';
+
 var jasonBaker = require('./lib/jason-baker');
+
+jasonBaker.promise = function jasonBakerPromise(recipe, options) {
+  /* eslint no-undef: 0 */
+  return new Promise(function makePromise(party, poop) {
+    jasonBaker(recipe, options, function whatsPromised(flop, cake) {
+      return flop === null ? party(cake) : poop(flop);
+    });
+  });
+};
+
 module.exports = jasonBaker;
